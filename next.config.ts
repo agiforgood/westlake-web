a0235bcd4ee80faa7ec1a6036a4c88d5ecc9d6d7
indirasieben <indirasiebenenv79@gmail.com>
2025-05-15 05:05:10 +0800

    feat: 关闭eslint打包时校验

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+  eslint: {
+    // 在生产构建时忽略 ESLint 错误
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
