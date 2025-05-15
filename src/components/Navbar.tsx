9a2f8e44c698e0202fd75afc4b824311250e1ff7
david <>
2025-05-15 22:40:18 +0800

    chore: add chat entry in nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -132,6 +132,9 @@ export default function Navbar() {
                 <DropdownItem key="profile" href="/profile">
                   个人资料
                 </DropdownItem>
+                <DropdownItem key="profile" href="/chat">
+                  我的私信
+                </DropdownItem>
                 {profile?.role == "admin" ? (
                   <DropdownItem key="admin" href="/admin">
                     管理后台
